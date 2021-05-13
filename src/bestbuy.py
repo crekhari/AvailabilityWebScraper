0264f3dcff5336b921ba0e93e6df1f32ebfdaa45
Chiraag Rekhari <crekhari@gmail.com>
2021-05-13 03:43:11 -0400

    added explicit waits and replace implicit ones

--- src/bestbuy.py
+++ src/bestbuy.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 import random,pickle,os,time,sys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class BestBuyAutomation():
@@ -24,20 +27,25 @@ class BestBuyAutomation():
     def loadCookies(self,file, link):
         cookies = pickle.load(open(file, "rb"))
         print("adding cookies then for loop")
+        
         for cookie in cookies:
             self.driver.add_cookie(cookie)
         print("Cookies should be added")
+        
         self.driver.get(link)
 
     def checkCookies(self, file, link):
-        i = 1
         print("starting pickle testing")
+        # This try except checks if there is a cookies.pkl file.
+        # If there is not, then will create the file, login, and add cookies. 
+        # If there is the file, then the method will add the cookies into the web browser
         try:
             filesize = os.path.getsize(file)
             print(filesize)
             if filesize == 0:
                 print("File is there, but it is empty\nLogging in and then adding cookies")
                 self.login()
+                print("login complete")
                 self.addCookies(file)
             else:
                 print("File exists and is filled with cookies")
@@ -45,22 +53,21 @@ class BestBuyAutomation():
             print("File is not there\nFile is being created and cookies are being added to cookies.pkl\nLogging in and then adding cookies")
             self.login()
             self.addCookies(file)
-            if i <= 3:
-                self.checkCookies(file,link)
-                i+=1
-            else:
-                print("Cookies can not be loaded into cookies.pkl file.\nClosing application")
         self.loadCookies(file, link)
         
 
     def login(self):
         print("Sleeping for 10")
-        time.sleep(10)
-        dropdown = self.driver.find_element_by_xpath("//button[@class='btn-unstyled plButton account-button']//*[local-name()='svg']")
-        dropdown.click()
-        time.sleep(15)
-        signin= self.driver.find_element_by_link_text('Sign In')
-        signin.click()
+        dropdownClick = WebDriverWait(self.driver,15)
+        dropdownClick = (dropdownClick.until(EC.element_to_be_clickable((By.XPATH, "//button[@class='btn-unstyled plButton account-button']//*[local-name()='svg']")))).click()
+        # time.sleep(10)
+        # dropdown = self.driver.find_element_by_xpath("//button[@class='btn-unstyled plButton account-button']//*[local-name()='svg']")
+        # dropdown.click()
+        signInClick = WebDriverWait(self.driver,20)
+        signInClick = (signInClick.until(EC.element_to_be_clickable((By.LINK_TEXT, 'Sign In')))).click()
+        # time.sleep(15)
+        # signin= self.driver.find_element_by_link_text('Sign In')
+        # signin.click()
 
         username = self.driver.find_element_by_name('fld-e')
         password = self.driver.find_element_by_name('fld-p1')
@@ -68,9 +75,13 @@ class BestBuyAutomation():
         username.send_keys("crekhari@gmail.com")
         password.send_keys("bestbuyDrlal12#")
 
-        submit_btn = self.driver.find_element_by_xpath("/html/body/div[1]/div/section/main/div[2]/div[1]/div/div/div/div/form/div[4]")
-        submit_btn.click()
-        time.sleep(7)
+        submitBtnClick = WebDriverWait(self.driver,10)
+        submitBtnClick = (submitBtnClick.until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div/section/main/div[2]/div[1]/div/div/div/div/form/div[4]")))).click()
+
+        #WebDriverWait(self.driver, 10).until(lambda driver: driver.execute_script('return document.readyState') == 'complete')
+        # submit_btn = self.driver.find_element_by_xpath("/html/body/div[1]/div/section/main/div[2]/div[1]/div/div/div/div/form/div[4]")
+        # submit_btn.click()
+        time.sleep(5) # This is needed in order to let the whole page load so the cookies can be added
     
     def registerForQueue(self):
         boolean = True
