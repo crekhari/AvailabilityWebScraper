bb9fa79584727b62123d8d6c62f6f79f17cef065
Chiraag Rekhari <crekhari@gmail.com>
2021-05-14 14:57:13 -0400

    code is able to support multiple drivers

--- src/amazon.py
+++ src/amazon.py
@@ -1,38 +1,96 @@
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 import random,pickle,os,time,sys,requests
-
+from bs4 import BeautifulSoup
+from re import sub
+from decimal import Decimal
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class AmazonAutomation():
 
-    def __init__(self):
-        self.driver = webdriver.Chrome('/Users/chiraag/chromedriver')
+    def __init__(self, numDrivers):
+        #option = webdriver.ChromeOptions()
+        #option.add_argument('--disable-infobars')
+        self.List = []
+        for x in range(numDrivers):
+            self.List.append(webdriver.Chrome('/Users/chiraag/chromedriver'))
+        #self.driver = webdriver.Chrome('/Users/chiraag/chromedriver')#, options=option)
+        #self.driver2 = webdriver.Chrome('/Users/chiraag/chromedriver')
+        #self.List = [self.driver, self.driver2]
 
     def tearDown(self):
         self.driver.close()
 
     def setWebsiteLocation(self, homeLink):
-        self.driver.get(homeLink)
+        for driver in self.List:
+            driver.get(homeLink)
+        #self.driver.get(homeLink)
+        #self.driver2.get(homeLink)
         
     def executeTest(self,file, homeLink, link):
-        i=1
         self.setWebsiteLocation(homeLink)
-        #self.login(homeLink, link)
         self.checkCookies(file, homeLink, link)
-        # add to cart()
+        self.checkAvailability(homeLink, link)
+        #self.tearDown()
+        
+    def placeOrder(self):
+        self.driver.find_element_by_xpath("//input[@id='add-to-cart-button']").click()
+        self.driver.find_element_by_id("hlb-ptc-btn-native").click()
+        #self.driver.find_element_by_xpath("//input[@name='placeYourOrder1']").click()
+        print("Item order has been placed")
+
+    def checkAvailability(self, homeLink, link):
+        boolean = True
+        while(boolean):
+            k = random.randint(5, 15)
+            passDriver = None
+            
+            try:
+                for driver in self.List:
+                    passDriver = driver
+                    price = self.getPrice(driver)
+                    if(price > 1900 or price < 1500):
+                        print(passDriver)
+                        print("Reached inside of or statment")
+                        print("sleeping now for " + str(k) + " seconds")
+                        time.sleep(k)
+                        passDriver.refresh()
+                    else:
+                        self.placeOrder()
+                        boolean = False
+            except Exception:
+                print("Could not find price")
+                print("sleeping exception for " + str(k) + " seconds")
+                time.sleep(k)
+                passDriver.refresh()
+
+    def getPrice(self, driver):
+        price = driver.find_element_by_id("priceblock_ourprice").text
+        intPrice = Decimal(sub(r'[^\d.]', '', price))
+        print(intPrice)
+        return intPrice
+    
 
     def addCookies(self, file):
-        pickle.dump(self.driver.get_cookies(), open(file,"wb"))
+        for driver in self.List:
+            pickle.dump(driver.get_cookies(), open(file,"wb"))
+        #pickle.dump(self.driver.get_cookies(), open(file,"wb"))
 
     def loadCookies(self,file, link):
         cookies = pickle.load(open(file, "rb"))
         print("adding cookies then for loop")
         
-        for cookie in cookies:
-            self.driver.add_cookie(cookie)
+        for driver in self.List:
+            for cookie in cookies:
+                driver.add_cookie(cookie)
+            driver.get(link)
+        #for cookie in cookies:
+            #self.driver.add_cookie(cookie)
         print("Cookies should be added")
         
-        self.driver.get(link)
+        #self.driver.get(link)
 
     def checkCookies(self, file, homeLink, link):
         print("starting checkCookies")
@@ -51,43 +109,29 @@ class AmazonAutomation():
                 print("File exists and is filled with cookies")
         except OSError as e:
             print("File is not there\nFile is being created and cookies are being added to cookies.pkl\nLogging in and then adding cookies")
-            self.login(homeLink,link)
+            self.login(homeLink,link, self.List[0])
             self.addCookies(file)
         self.loadCookies(file, link)
 
-        #cookieFile = open('cookies.txt', 'a')
-        #figure out cookies or just use beatiful soup but cookies will be easier and faster if you figure this out
-        #cookies = {}
-        #cookies = self.driver.get_cookies()
-        #for cookie in cookies:
-         #   if(cookies[cookie] == "www.amazon.com"):
-          #      print("FOUND IT")
-        #self.driver.add_cookie({"name": "foo", "value": "bar"})
-        #print(self.driver.get_cookie("foo"))
-    
-    def checkLogin(self, homeLink, link):
-        i=1
 
-
-
-    def login(self, homeLink, link):
-        signInBtn = self.driver.find_element_by_xpath("//span[@class='nav-line-2 nav-long-width']")
+    def login(self, homeLink, link, driver):
+        signInBtn = driver.find_element_by_xpath("//span[@class='nav-line-2 nav-long-width']")
         signInBtn.click()
 
-        username = self.driver.find_element_by_name('email')
+        username = driver.find_element_by_name('email')
         username.send_keys("crekhari@gmail.com")
-        usernameSubmit = self.driver.find_element_by_id('continue').click()
+        usernameSubmit = driver.find_element_by_id('continue').click()
 
-        password = self.driver.find_element_by_id('ap_password')
+        password = driver.find_element_by_id('ap_password')
         password.send_keys("Drlal12#")
-        passwordSumbit = self.driver.find_element_by_id('signInSubmit').click()
+        driver.find_element_by_name("rememberMe").click()
+        passwordSumbit = driver.find_element_by_id('signInSubmit').click()
 
-        time.sleep(3)
+        time.sleep(3) #DO NOT REMOVE --  This is needed in order for the cookies to load properly
 
-    
-
-    
+    #https://www.amazon.com/EVGA-GeForce-Technology-Backplate-24G-P5-3987-KR/dp/B08J5F3G18
 
 if __name__ == "__main__":
-    taskmaster = AmazonAutomation()
-    taskmaster.executeTest("amazonCookies.pkl", 'https://www.amazon.com', 'https://www.amazon.com/EVGA-GeForce-Technology-Backplate-24G-P5-3987-KR/dp/B08J5F3G18')
+    numDrivers = 2
+    taskmaster = AmazonAutomation(numDrivers)
+    taskmaster.executeTest("amazonCookies.pkl", 'https://www.amazon.com', 'https://www.amazon.com/gp/product/B079KYZ9FW?pf_rd_r=4ZYXRC10NATBQ9J26K1P&pf_rd_p=5ae2c7f8-e0c6-4f35-9071-dc3240e894a8&pd_rd_r=39d8b0c5-6ca8-41a8-973c-da08ddc08960&pd_rd_w=dcGK4&pd_rd_wg=cUqPu&ref_=pd_gw_unk')
